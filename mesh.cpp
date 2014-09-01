49ff93a94332dafd3bef7d8324e561a27d9b3ea6
myrovh <myrovh@yandex.com>
2014-09-01 23:19:57 +1000

    mesh class created

--- mesh.cpp
+++ mesh.cpp
@@ -3,9 +3,89 @@
 
 mesh::mesh()
 {
+	reference_count = 0;
+	material_buffer = NULL;
+	mesh_storage = NULL;
+	number_of_materials = NULL;
+	materials = NULL;
+	mesh_textures = NULL;
+	filename = NULL;
 }
 
-
-mesh::~mesh()
+bool mesh::load(LPDIRECT3DDEVICE9 device, const char* filename, texture_manager* texture_manage)
 {
+	if(FAILED(D3DXLoadMeshFromX(filename, D3DXMESH_VB_SHARE, device, NULL, &material_buffer, NULL, &number_of_materials, &mesh_storage)))
+	{
+		return FALSE;
+	}
+
+	this->filename = filename;
+	materials = new D3DMATERIAL9[number_of_materials];
+	mesh_textures = new texture*[number_of_materials];
+	D3DXMATERIAL* loaded_materials = (D3DXMATERIAL*)material_buffer->GetBufferPointer();
+
+	for(DWORD i = 0; i < number_of_materials; i++)
+	{
+		materials[i] = loaded_materials[i].MatD3D;
+		materials[i].Ambient = materials[i].Diffuse;
+
+		char* texture_filename = loaded_materials[i].pTextureFilename;
+		if(texture_manage->load(device, texture_filename))
+		{
+			mesh_textures[i] = texture_manage->get_texture(texture_filename);
+		}
+		else
+		{
+			mesh_textures[i] = NULL;
+		}
+	}
+	return TRUE;
+}
+
+void mesh::release(texture_manager* texture_manage)
+{
+	for(DWORD i = 0; i < number_of_materials; i++)
+	{
+		if(mesh_textures[i] != NULL)
+		{
+			texture* texture = mesh_textures[i];
+			texture_manage->release_texture(texture);
+			mesh_textures[i] = NULL;
+		}
+	}
+
+	if(materials != NULL)
+	{
+		delete[] materials;
+		materials = NULL;
+		number_of_materials = 0;
+	}
+
+	if(mesh_textures != NULL)
+	{
+		delete[] mesh_textures;
+		mesh_textures = NULL;
+	}
+
+	if(material_buffer != NULL)
+	{
+		material_buffer->Release();
+		material_buffer = NULL;
+	}
+
+	if(mesh_storage != NULL)
+	{
+		mesh_storage->Release();
+		mesh_storage = NULL;
+	}
+}
+
+void mesh::render(LPDIRECT3DDEVICE9 device)
+{
+	for(DWORD i = 0; i < number_of_materials; i++)
+	{
+		device->SetMaterial(&materials[i]);
+		device->SetTexture(0, mesh_textures[i]->get_texture());
+		mesh_storage->DrawSubset(i);
+	}
 }
--- mesh.h
+++ mesh.h
@@ -1,15 +1,61 @@
 #ifndef MESH_H
 #define MESH_H
-#include "object.h"
+#include <d3dx9.h>
+#include "texture_manager.h"
 
-class mesh :
-	public object
+class mesh
 {
 private:
+	int reference_count;
+	LPD3DXBUFFER material_buffer;
+	LPD3DXMESH mesh_storage;
+	DWORD number_of_materials;
+	D3DMATERIAL9* materials;
+	texture** mesh_textures;
+	const char* filename;
+
+	void release(texture_manager* texture_manage);
+
+protected:
+	mesh();
+	~mesh() {};
+
+	void add_reference()
+	{
+		reference_count++;
+	}
+	void remove_reference()
+	{
+		reference_count--;
+	}
+	int get_reference_count()
+	{
+		return reference_count;
+	}
 
 public:
-	mesh();
-	virtual ~mesh();
+	bool load(LPDIRECT3DDEVICE9 device, const char* filename, texture_manager* texture_manage);
+
+	const char* get_filename()
+	{
+		return filename;
+	}
+	LPD3DXMESH get_d3d_mesh()
+	{
+		return mesh_storage;
+	}
+	D3DMATERIAL9* get_mesh_material(int index)
+	{
+		return &materials[index];
+	}
+	texture* get_mesh_texture(int index)
+	{
+		return mesh_textures[index];
+	}
+
+	void render(LPDIRECT3DDEVICE9 device);
+
+	friend class mesh_manager;
 };
 
 #endif
