25487418a6b55292d17e1de39018ca3ff771775d
myrovh <myrovh@yandex.com>
2014-09-15 12:35:21 +1000

    removed references to yathzee game code

--- game.h
+++ game.h
@@ -7,81 +7,12 @@
 #include "mesh_manager.h"
 #include "object.h"
 #include "direct3d.h"
-#include "die.h"
 #include "camera_fixed.h"
 #include "font_rectangle.h"
 #include <windows.h>
 #include <stdio.h> //vsprintf
 #include <vector> //object_queue
-#include <list>
 #include <sstream> //font insertion
-#include <random>
-#include <chrono>
-
-enum yahtzee_types {
-	ONES, 
-	TWOS, 
-	THREES, 
-	FOURS, 
-	FIVES, 
-	SIXES, 
-	BONUS,
-	KIND_3,
-	KIND_4,
-	HOUSE,
-	S_STRAIGHT,
-	L_STRAIGHT,
-	YAHTZEE,
-	CHANCE
-};
-
-struct yahtzee_game
-{
-	// START State Settings
-	bool is_game_over;
-	bool is_score_for_round_set;
-	int round_count;
-	int rolls_remaining;
-	int max_rolls;
-	// END State Settings
-
-	//START Physics Settings
-	double throw_variance_floor;
-	double throw_variance_ceiling;
-	float throw_entropy;
-	float throw_force;
-	float gravity_force;
-	D3DXVECTOR3 throw_direction;
-	//END Physics Settings
-
-	// START upper section
-	int bonus_threshold;
-	int bonus_value;
-	int bonus_score;
-
-	int ones_score;
-	int twos_score;
-	int threes_score;
-	int fours_score;
-	int fives_score;
-	int sixes_score;
-	// END upper section
-
-	// START lower section
-	int full_house_value;
-	int small_straight_value;
-	int large_straight_value;
-	int yahtzee_value;
-
-	int three_of_a_kind_score;
-	int four_of_a_kind_score;
-	int full_house_score;
-	int small_straight_score;
-	int large_straight_score;
-	int yahtzee_score;
-	int chance_score;
-	// END lower section
-};
 
 class game
 {
@@ -93,7 +24,6 @@ private:
 	camera_fixed* camera;
 	std::vector<object*> object_queue;
 	std::vector<font_rectangle*> font_queue;
-	yahtzee_game game_variables;
 
 public:
 	game();
@@ -104,13 +34,6 @@ public:
 	void update(float timestep);
 	void render();
 	void trace(const char * fmt, ...);
-
-	void roll_dice();
-	void new_round();
-	bool check_game_over();
-	int calculate_yahtzee_values(yahtzee_types value_to_return);
-	int calculate_final_score();
-	bool assign_yahtzee_values(yahtzee_types value_to_set);
 };
 
 #endif
