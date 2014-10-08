b0597ec25083689df128a74c3d71c138025eab13
myrovh <myrovh@yandex.com>
2014-10-08 12:32:58 +1100

    third person camera now follows target up not world up

--- Camera_Third.cpp
+++ Camera_Third.cpp
@@ -19,22 +19,27 @@ Camera_Third::Camera_Third(D3DXVECTOR3 position, Object* look_at_target, D3DXVEC
 
 D3DXMATRIX Camera_Third::get_view()
 {
+	//Calculate values
 	D3DXMATRIX view;
 	D3DXQUATERNION rotation = look_at_target->get_object_rotation();
 	D3DXVECTOR3 look_at_position = look_at_target->get_object_location();
 	D3DXQUATERNION rotation_inverse;
 	D3DXQuaternionConjugate(&rotation_inverse, &rotation);
 	D3DXQUATERNION offset_quaternion(position.x, position.y, position.z, 0.0f);
-
 	D3DXQUATERNION rotated_offset = rotation_inverse * offset_quaternion * rotation;
 
+	//Set camera position to target location minus offset rotated with the same rotation as target
 	D3DXVECTOR3 offset_vector(rotated_offset.x, rotated_offset.y, rotated_offset.z);
 	offset_vector = -offset_vector;
 	D3DXVECTOR3 camera_location = offset_vector + look_at_position;
 
+	//Set up direction to target up instead of world up
+	D3DXVECTOR3 camera_up = up_direction;
+	D3DXMATRIX transform_up;
+	D3DXMatrixRotationQuaternion(&transform_up, &rotation);
+	D3DXVec3TransformCoord(&camera_up, &camera_up, &transform_up);
 
-
-	D3DXMatrixLookAtLH(&view, &camera_location, &look_at_position, &up_direction);
+	D3DXMatrixLookAtLH(&view, &camera_location, &look_at_position, &camera_up);
 	return view;
 }
 
